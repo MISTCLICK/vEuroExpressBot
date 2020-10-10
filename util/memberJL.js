898ef5ccc6c9709df71d194f7bac0e99c8bb9655
MISTCLICK <56761784+MISTCLICK@users.noreply.github.com>
2020-10-10 21:59:14 +0300

    Update memberJL.js

--- util/memberJL.js
+++ util/memberJL.js
@@ -1,7 +1,7 @@
 module.exports = client => {
     client.on('guildMemberAdd', async (member) => {
         const channel = client.channels.cache.get('731136189272162314')
-        channel.send(`**${member.displayName}**` +  ' just joined the server!\nWelcome to the EuroExpress Virtual official server! Please, read carefully our <#731125322992320532> and wait for your activation. You may DM `@Grigorii A EXP001` or `@Artur Vasiljev EXP008` for that');
+        channel.send(`**${member.displayName}**` +  ' just joined the server!\nWelcome to the EuroExpress Virtual official server! Please, read carefully our <#731125322992320532> and wait for your activation. You may DM `Grigorii A EXP001` or `Artur Vasiljev EXP008` for that');
 
         member.roles.add('731130774442737807')
     });
