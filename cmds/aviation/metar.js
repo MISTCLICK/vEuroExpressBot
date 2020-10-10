0bcd64822c00e278a7c83524c131eb36d0c739b9
MISTCLICK <56761784+MISTCLICK@users.noreply.github.com>
2020-10-10 22:09:55 +0300

    Metar and TAF now support lowercase letters

--- cmds/aviation/metar.js
+++ cmds/aviation/metar.js
@@ -7,18 +7,25 @@ module.exports = class MetarCommand extends Commando.Command {
             name: 'metar',
             group: 'aviation',
             memberName: 'metar',
-            description: 'Provides the METAR of the airport requested.'
+            description: 'Provides the METAR of the airport requested.',
+            args: [
+                {
+                    key: 'airport',
+                    prompt: "What airport's metar would you like to get?",
+                    type: 'string'
+                }
+            ]
         })
     }
 
-    async run(message, args) {
+    async run(message, airport) {
             let getMetar = async () => {
-            let response = await axios.get(`https://metartaf.ru/${args}.json`)
+            let response = await axios.get(`https://metartaf.ru/${airport.toUpperCase()}.json`)
             let metar = response.data
             return metar
         }
         let metarValue = await getMetar();
-        console.log(`Metar of ${args} has been requested!`)
+        console.log(`Metar of ${airport.toUpperCase()} has been requested!`)
         console.log(metarValue);
         message.reply("```" + `\n${metarValue.metar.slice(19)}` + "\n```");
     }
--- cmds/aviation/taf.js
+++ cmds/aviation/taf.js
@@ -7,18 +7,25 @@ module.exports = class TafCommand extends Commando.Command {
             name: 'taf',
             group: 'aviation',
             memberName: 'taf',
-            description: 'Provides the TAF of the airport requested.'
+            description: 'Provides the TAF of the airport requested.',
+            args: [
+                {
+                    key: 'airport',
+                    prompt: "What airport's metar would you like to get?",
+                    type: 'string'
+                }
+            ]
         })
     }
 
-    async run(message, args) {
+    async run(message, airport) {
                 let getTAF = async () => {
-            let response = await axios.get(`https://metartaf.ru/${args}.json`)
+            let response = await axios.get(`https://metartaf.ru/${airport.toUpperCase()}.json`)
             let taf = response.data
             return taf
         }
         let tafValue = await getTAF();
-        console.log(`TAF of ${args} has been requested!`)
+        console.log(`TAF of ${airport.toUpperCase()} has been requested!`)
         console.log(tafValue);
         message.reply("```" + `\n${tafValue.taf.slice(19)}` + "\n```");
     }
