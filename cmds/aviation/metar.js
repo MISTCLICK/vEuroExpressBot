5d8bcadf8e615db8bf0a1faf7a38c1476f95c84d
MISTCLICK <56761784+MISTCLICK@users.noreply.github.com>
2020-10-10 23:23:07 +0300

    More bugs fixed. Especially METAR and TAF

--- cmds/aviation/metar.js
+++ cmds/aviation/metar.js
@@ -20,12 +20,15 @@ module.exports = class MetarCommand extends Commando.Command {
 
     async run(message, airport) {
             let getMetar = async () => {
-            let response = await axios.get(`https://metartaf.ru/${airport.toUpperCase()}.json`)
-            let metar = response.data
-            return metar
+                let airportString = airport.airport;
+                let airportCode = airportString.toUpperCase();
+                let metarURL = 'https://metartaf.ru/'+airportCode+'.json'
+                let response = await axios.get(metarURL);
+                let metar = response.data;
+                return metar;
         }
         let metarValue = await getMetar();
-        console.log(`Metar of ${airport.toUpperCase()} has been requested!`)
+        console.log(`Metar of ${airport.airport} has been requested!`)
         console.log(metarValue);
         message.reply("```" + `\n${metarValue.metar.slice(19)}` + "\n```");
     }
--- cmds/aviation/taf.js
+++ cmds/aviation/taf.js
@@ -19,13 +19,15 @@ module.exports = class TafCommand extends Commando.Command {
     }
 
     async run(message, airport) {
-                let getTAF = async () => {
-            let response = await axios.get(`https://metartaf.ru/${airport.toUpperCase()}.json`)
+            let getTAF = async () => {
+            let airportString = airport.airport
+            let airportCode = airportString.toUpperCase()
+            let response = await axios.get(`https://metartaf.ru/${airportCode}.json`)
             let taf = response.data
             return taf
         }
         let tafValue = await getTAF();
-        console.log(`TAF of ${airport.toUpperCase()} has been requested!`)
+        console.log(`TAF of ${airport.airport} has been requested!`)
         console.log(tafValue);
         message.reply("```" + `\n${tafValue.taf.slice(19)}` + "\n```");
     }
